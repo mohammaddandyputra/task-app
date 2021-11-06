645e6a2a129e24b2f80f5bd678abb6a0fa779c9b
Mohammad Dandy Putra <dandy@polaritasgroup.com>
2021-11-06 08:21:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,70 @@
-import React from "react";
+import React, {useState} from 'react';
+import {Button, TextField } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import "./App.css";
 
-function HelloWorld(){
-  return <div style={{backgroundColor: 'blue'}}>Hello world</div>
+const Header = () => {
+  return <>
+    <h1>Task App</h1>
+  </>
 }
 
-function Button(){
-  return <button onClick={() => alert('Selamat Datang')}>Coba klik saya</button>
+const InfoBar = ({taskNumber}) => {
+  return <div>Ada {taskNumber} yang perlu diselesaikan</div>
 }
 
-function App(){
-  return <div>
-    <HelloWorld/>
-    <Button/>
-    
+const TaskAdder = ({setTasks, tasks}) => {
+  const [currentValue, setCurrentValue] = useState('');
+
+  const handleAddTask = () => {
+    const newTasks = {
+      id: tasks.length + 1,
+      message: currentValue
+    }
+    setTasks([...tasks, newTasks])
+    setCurrentValue('');
+  }
+  return <div className="task-adder">
+    <TextField value={currentValue} color="secondary" onChange={event => setCurrentValue(event.target.value)} label="Tambah task" variant="outlined" focused />
+    <Button variant="contained" color="primary"  disabled={currentValue === ''} onClick={() => handleAddTask()}>Tambah</Button>
   </div>
+}
+
+const Task = ({message, id, setTasks, tasks}) => {
+  const handleDelete = () => {
+    const updateTasks = tasks.filter(task => task.id !== id);
+    setTasks(updateTasks);
+  }
+  return <div className="container-task">
+    <div className="task">
+    <div style={{display: "inline-block"}}>{message}</div>
+    <Button variant="contained" color="secondary" startIcon={<DeleteIcon />} onClick= { () => handleDelete()} id={id}>Delete</Button>
+    </div>
+  </div>
+}
+
+const TaskLisk = ({tasks, setTasks}) => {
+  return tasks.map(task => {
+    return <Task message={task.message} id={task.id} setTasks={setTasks} tasks={tasks} />
+  })
+}
+
+const TaskApp = () => {
+  const [tasks, setTasks] = useState([]);
+  return <div className="container">
+    <p>
+      <Header />
+      <InfoBar taskNumber={tasks.length}/>
+      <TaskAdder setTasks={setTasks} tasks={tasks}/>
+      <TaskLisk tasks={tasks} setTasks={setTasks}/>
+    </p>
+  </div>
+}
+
+const App = () => {
+  return <>
+    <TaskApp />
+  </>
 }
 
 export default App;
